afae91df7ca7fc8b8c400e061e2c5febf307d0a9
AbdullahMPrograms <abdullahmalik1385@gmail.com>
2023-10-24 21:12:52 -0400

    Lab 2 typeup fix

---
+++ Lab6/src/coe318/lab6/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    private int id;
+    private static int nextId = 0;  //why do we need this? instructions say we need it but cant we just this.id++ in Node()?
+
+    public Node() {
+        this.id = nextId;
+        nextId++;
+    }
+    
+    @Override
+    public String toString() {
+        return "" + id;
+    }
+}
