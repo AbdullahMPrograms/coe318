9feff856698fb6650a2579f65b9c093c95d716c7
AbdullahMPrograms <abdullahmalik1385@gmail.com>
2023-11-13 21:00:30 -0500

    scanner catch fix

--- Lab7/src/coe318/lab7/UserMain.java
+++ Lab7/src/coe318/lab7/UserMain.java
@@ -9,30 +9,35 @@ import java.util.Scanner;
 
 public class UserMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Circuit circuit = new Circuit();
-        String input;
+        try (Scanner scanner = new Scanner(System.in)) {
+            Circuit circuit = new Circuit();
+            String input;
 
-        while (true) {
-            input = scanner.nextLine();
-            String[] parts = input.split(" ");
+            while (true) {
+                input = scanner.nextLine();
+                String[] parts = input.split(" ");
 
-            switch (parts[0]) {
-                case "r":
-                    circuit.addResistor(new Resistor(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Double.parseDouble(parts[3])));
-                    break;
-                case "v":
-                    circuit.addVoltageSource(new VoltageSource(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Double.parseDouble(parts[3])));
-                    break;
-                case "spice":
-                    circuit.printSpice();
-                    break;
-                case "end":
-                    System.out.println("All Done");
-                    return;
-                default:
-                    System.out.println("Invalid command");
+                switch (parts[0]) {
+                    case "r":
+                        circuit.addResistor(new Resistor(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Double.parseDouble(parts[3])));
+                        break;
+                    case "v":
+                        circuit.addVoltageSource(new VoltageSource(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Double.parseDouble(parts[3])));
+                        break;
+                    case "spice":
+                        circuit.printSpice();
+                        break;
+                    case "end":
+                        System.out.println("All Done");
+                        return;
+                    default:
+                        System.out.println("Invalid command");
+                }
             }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
+
